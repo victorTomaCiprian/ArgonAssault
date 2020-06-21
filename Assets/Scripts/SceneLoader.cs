b4da8cc95e5bf59372f850237be668ef1b49fabe
Victor <victor.toma@evozon.com>
2020-06-21 03:31:38 +0300

    Added Splash screen and music

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour{
+	#region Variables
+    //Config
+    [SerializeField] private float _splashScreenDuration = 5f;
+    //State
+
+    //Cached component references
+    private int _currentSceneIndex;
+	#endregion
+	
+	#region Unity Methods
+    private void Awake() {
+	    _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+    
+    private void Start() {
+	    StartCoroutine(LoadNextSceneRoutine(_splashScreenDuration));
+    }
+
+    private void Update(){
+        
+    }
+	#endregion
+	
+
+	private IEnumerator LoadNextSceneRoutine(float timeBetweenScenes = 0f) {
+		yield return new WaitForSecondsRealtime(timeBetweenScenes);
+		SceneManager.LoadScene(_currentSceneIndex + 1);
+	}
+}
